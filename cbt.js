cd3e4124788bb8f44e215db66275defb28a6b2b4
toChaim <toChaim@gmail.com>
2017-07-02 19:29:56 -0700

    compleat rewrite, swich from confirm to use a form.

--- cbt.js
+++ cbt.js
@@ -1,32 +1,12 @@
-$( document ).ready(function() {
-	console.log("document ready");
+$(document).ready(function(){
+	console.log("document ready")
 
-    var Prompt = function(prom, conf, logs = []){
-    	this.prompt = prom;
-    	this.confirm = conf;
-    	this.display = function(){
-    		var ans = confirm(this.prompt);
-    		if(ans == this.confirm){
-    			console.log("got it right");
-    			$("#score").html(parseInt($("#score").html())+1);
-    		}else{
-    			console.log("got it wrong");
-    			$("#score").html(parseInt($("#score").html())-1);
-    		}
-    	};
-    	this.logics = logs;
-    }
+	console.log("constructors and lists");
 
-    var Bout = function(prom, time, resp){
-    	this.prompt = prom;
-    	this.time = time;
-    	this.resp = resp;
-    }
-
-    var Logic = function(log, des){
-    	this.logic = log;
-    	this.description = des;
-    };
+	function Logic(title, txt){
+		this.title = title;
+		this.text = txt;
+	}
 
 	var logics = [
 		new Logic("Black and White", "Things are rarely all one way or another."),
@@ -35,37 +15,190 @@ $( document ).ready(function() {
 		new Logic("Comparing up insted of down.", "When we look at people who have more we see our lack. "
 			+ "When we look people who have less we see some of what we have."),
 		new Logic("Things change", "Impermanance as Budihst say."),
+		new Logic("Taking part for the whole", "That guy looks good (right now)." 
+			+ " She is (looks) confident (in this conversation)..." 
+			+ " I wish I could be handsom and confident like them."
+			+ " This take a little known information and ignores all the things that are not known."),
 		
-		
-		];
 
+	];
 
+	var txtHTML = "";
 	for(var i of logics){
-		$("#logic").append("<li>" + i.logic + "</li>")
+		txtHTML += "<tr><td>"
+		+ i.title + "</td><td>"
+		+ i.text + "</td><td>"
+		+ "0</td><td>"
+		+ "1:00</td></tr>";
+	}
+	$("#logics tr:last").after(txtHTML);
+
+	function Response(txt, confirm){
+		this.text = txt;
+		this.confirm = confirm;
+	}
+
+	var responses = [
+		new Response("Yes", true),
+		new Response("No", false),
+	];
+
+	function Prompt(txt, confirm, response = [], logic = []){
+		this.text = txt;
+		this.confirm = confirm;
+		this.responses = response;
+		this.logics = logic;
 	}
 
 	var prompts = [
-		new Prompt("You are willing to feel better!", true),
-		new Prompt("You are the worst person in the world.", false),
-		new Prompt("You are willing to change and grow!", true),
-		new Prompt("You are brave!", true),
-		new Prompt("Almost everything people do are skills to be learned.", true),
-		new Prompt("You never do anything right!", false),
-		];
+		new Prompt("You are willing to play.", true, [responses[0], responses[1]], []),
+		new Prompt("You are willing to feel better!", true, [responses[0], responses[1]], []),
+		new Prompt("You are the worst person in the world.", false, [responses[0], responses[1]], []),
+		new Prompt("You are willing to change and grow!", true, [responses[0], responses[1]], []),
+		new Prompt("You are brave!", true, [responses[0], responses[1]], []),
+		new Prompt("Almost everything people do are skills to be learned.", true, [responses[0], responses[1]], []),
+		new Prompt("You never do anything right!", false, [responses[0], responses[1]], []),
+	];
+
+	var txtHTML = "";
+	for(var p of prompts){
+		txtHTML += "<tr><td>"
+		+ p.text + "</td><td>"
+		+ p.confirm + "</td><td>"
+		+ "0</td><td>"
+		+ "1:00</td></tr>";
+	}
+	$("#prompts tr:last").after(txtHTML);
+
+
+	function Bout(prompt, responses, pick = null, time = 0){
+		this.prompt = prompt;
+		this.responses = responses;
+		this.pick = pick;
+		this.time = time;
+	}
 
 	var bouts = [];
 
-	for(var p of prompts){
-		$("#prompts").append("<li>" + p.prompt + "</li>")
-	}
 
-	$("#plybtn").click(function(){
-		console.log("plybtn click");
-		for(var i = 0; i < 5; i++){
-			var prom = prompts[Math.floor(Math.random() * prompts.length)];
-			var tim = setTimeout(function(){prom.display();}, 1);
+	console.log("click handling");
 
+	$(".response").click(function(){
+		$this = $(this);
+		var bout = bouts[bouts.length-1];
+		var index = parseInt($this.attr('id').substring(3));
+
+		var notPlaying = $this.hasClass("afirm") === false && $this.hasClass("refuse") === false;
+		notPlaying = notPlaying && bout === undefined;
+		if(notPlaying) {
+			console.log("notPlaying");
+			return;
+		}
+
+		var $score = $("#score");
+
+		if(
+			($this.hasClass("afirm") && bout.prompt.confirm)
+			|| ($this.hasClass("refuse") && !bout.prompt.confirm)
+			){
+			bout.pick = index;
+			console.log("You win");
+			$score.html(parseInt($score.html()) + 1);
+		}else if(
+			($this.hasClass("afirm") && !bout.prompt.confirm)
+			|| ($this.hasClass("refuse") && bout.prompt.confirm)
+			){
+			bout.pick = index;
+			console.log("You loose");
+			$score.html(parseInt($score.html()) - 1);
+		}else{
+			console.log("Should neve run.");
+		}
+
+		clear();
+		display(new Bout(randomPick(prompts), [responses[0], responses[1]]));
+	});
+
+	$("#playstop").click(function(){
+		console.log("playstop click");
+		if($("#playstop").html() === "Play"){
+			$("#playstop").html("Stop");
+			
+			var prom = randomPick(prompts);
+
+			var bout = new Bout(
+				prom,
+				[responses[0], responses[1]]
+				);
+			display(bout);
+		}else{
+			$("#playstop").html("Play");
+			clear();
 		}
 	});
 
+	console.log("application helper functions");
+
+	//later should have stuff about user and level
+	function display(bout){	
+			bouts.push(bout);
+
+			var $res = $("#res0");
+			$res.html(bout.responses[0].text);
+			if(bout.responses[0].confirm === true){
+				$res.removeClass("refuse");
+				$res.addClass("afirm");
+			}else{
+				$res.addClass("refuse");
+				$res.removeClass("afirm");
+			}
+			$res = $("#res1");
+			$res.html(bout.responses[1].text);
+			if(bout.responses[1].confirm === true){
+				$res.removeClass("refuse");
+				$res.addClass("afirm");
+			}else{
+				$res.addClass("refuse");
+				$res.removeClass("afirm");
+			}
+			$("#prompt").html(bout.prompt.text);
+	}
+	function clear(){
+		console.log("clear");
+		var $res = $(".response");
+		var bout = bouts[bouts.length-1];
+		if(bout !== undefined && bout.pick === null){
+			bouts.pop();
+		}
+		for(var i = 0; i < $res.length; i++){
+
+		}
+	}
+
 });
+
+//adapted from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function arrayShuffle(arr){
+	var array = [];
+	for(var i = 0; i < arr.length; i++) array.push(arr[i]);
+
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+function randomPick(arr){
+	return arr[Math.floor(Math.random() * arr.length)];
+}
